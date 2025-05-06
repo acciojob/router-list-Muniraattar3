4c305d931305b66f360f9d91df8e25f2fbab5e3d
Munira03 <muniraattar17@gmail.com>
2025-05-06 17:22:02 +0530

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React from "react"
-import './../styles/App.css;
-  
-const App=()=> {
-  return (
-  <div>
-    
-    </div>
-  )
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ItemList />} />
+      <Route path="/items/:id" element={<ItemDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
